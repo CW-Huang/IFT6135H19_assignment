9896bb18b5537b5900fce6efa921ac00c443b340
teganmaharaj <tegan.jrm@gmail.com>
2019-03-08 15:45:01 -0500

    updated train and val for transformer experiment

--- assignment2/ptb-lm.py
+++ assignment2/ptb-lm.py
@@ -49,7 +49,7 @@
 #      perplexities:
 #                  RNN: train:  120  val: 157
 #                  GRU: train:   65  val: 104
-#          TRANSFORMER:  train:  77  val: 152
+#          TRANSFORMER:  train:  67  val: 146
 #    - For Problem 4.2 (exploration of optimizers), you will make use of the 
 #      experiments from 4.1, and should additionally run the following experiments:
 #          --model=RNN --optimizer=SGD --initial_lr=0.0001 --batch_size=20 --seq_len=35 --hidden_size=1500 --num_layers=2 --dp_keep_prob=0.35 
