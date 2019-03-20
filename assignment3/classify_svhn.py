7a50ce21eb04cb2e3a6b9dcaa9d9d55c5b9ca318
Shawn Tan <shawn@wtf.sg>
2019-03-20 15:06:07 -0400

    Code for classification.

--- assignment3/classify_svhn.py
+++ assignment3/classify_svhn.py
@@ -3,33 +3,27 @@ import torch
 import torchvision.datasets
 import torchvision.transforms as transforms
 from torch.utils.data import dataset
-import os
-import numpy as np
 from torch import nn
-from torch.nn.modules import upsampling
-from torch.functional import F
+# from torch.nn.modules import upsampling
+# from torch.functional import F
 from torch.optim import Adam
 
 
 def get_data_loader(dataset_location, batch_size):
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0., 0., 0.), (1., 1., 1.))
+    ])
     trainvalid = torchvision.datasets.SVHN(
         dataset_location, split='train',
         download=True,
-        transform=transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-        ])
+        transform=transform
     )
-    trainset_size = int(len(trainvalid) * 0.8)
+
+    trainset_size = int(len(trainvalid) * 0.9)
     trainset, validset = dataset.random_split(
         trainvalid,
         [trainset_size, len(trainvalid) - trainset_size]
-    )
-
-    testset = torchvision.datasets.SVHN(
-        dataset_location, split='test',
-        transform=None, target_transform=None,
-        download=True
     )
 
     trainloader = torch.utils.data.DataLoader(
@@ -44,56 +38,100 @@ def get_data_loader(dataset_location, batch_size):
         batch_size=batch_size,
     )
 
-    return trainloader, validloader
+    testloader = torch.utils.data.DataLoader(
+        torchvision.datasets.SVHN(
+            dataset_location, split='test',
+            download=True,
+            transform=transform
+        ),
+        batch_size=batch_size,
+    )
 
+    return trainloader, validloader, testloader
 
 
 class Classifier(nn.Module):
     def __init__(self):
         super(Classifier, self).__init__()
         self.conv_stack = nn.Sequential(
-            nn.Conv2d(3, 16, 3, padding=1),
-            nn.ReLU(),
-            nn.Dropout(0.5),
+            nn.Conv2d(3, 8, 3, padding=1),
+            nn.ELU(),
+            nn.Dropout2d(p=0.1),
+            nn.Conv2d(8, 16, 3, padding=1),
+            nn.ELU(),
+            nn.Dropout2d(p=0.1),
             nn.MaxPool2d(2),
+
+            nn.Conv2d(16, 16, 3, padding=1),
+            nn.ELU(),
+            nn.Dropout2d(p=0.1),
             nn.Conv2d(16, 32, 3, padding=1),
-            nn.ReLU(),
-            nn.Dropout(0.5),
+            nn.ELU(),
+            nn.Dropout2d(p=0.1),
             nn.MaxPool2d(2),
-            nn.Conv2d(32, 64, 3, padding=1),
-            nn.ReLU(),
-            nn.Dropout(0.5),
+
+            nn.Conv2d(32, 32, 3, padding=1),
+            nn.ELU(),
+            nn.Dropout2d(p=0.1),
+            nn.Conv2d(32, 128, 3, padding=1),
+            nn.ELU(),
+            nn.Dropout2d(p=0.1),
             nn.MaxPool2d(2),
-            nn.Conv2d(64, 128, 3, padding=1),
-            nn.ReLU(),
-            nn.Dropout(0.5),
-            nn.MaxPool2d(2),
-            nn.Conv2d(128, 256, 2),
+
+            nn.Conv2d(128, 512, 2),
         )
 
         self.mlp = nn.Sequential(
-            nn.ReLU(),
+            nn.ELU(),
             nn.Dropout(0.5),
-            nn.Linear(256, 10),
+            nn.Linear(512, 10),
         )
+        for p in self.parameters():
+            if p.dim() > 1:
+                nn.init.xavier_uniform_(p)
 
     def forward(self, x):
         return self.mlp(self.extract_features(x))
 
     def extract_features(self, x):
-        return self.conv_stack(x).view(-1, 256)
+        return self.conv_stack(x)[:, :, 0, 0]
+
+
+def evaluate(classify, dataset):
+    with torch.no_grad():
+        classify.eval()
+        correct = 0.
+        total = 0.
+        for x, y in dataset:
+            if cuda:
+                x = x.cuda()
+                y = y.cuda()
+
+            c = (classify(x).argmax(dim=-1) == y).sum().item()
+            t = x.size(0)
+            correct += c
+            total += t
+    acc = correct / float(total)
+    return acc
 
 
 if __name__ == "__main__":
-    train, valid = get_data_loader("svhn", 32)
+    train, valid, test = get_data_loader("svhn", 32)
     classify = Classifier()
     params = classify.parameters()
     optimizer = Adam(params)
     ce = nn.CrossEntropyLoss()
     best_acc = 0.
-    for _ in range(10):
+    cuda = torch.cuda.is_available()
+    if cuda:
+        classify = classify.cuda()
+
+    for _ in range(50):
         classify.train()
         for i, (x, y) in enumerate(train):
+            if cuda:
+                x = x.cuda()
+                y = y.cuda()
             out = classify(x)
             loss = ce(out, y)
             loss.backward()
@@ -101,19 +139,12 @@ if __name__ == "__main__":
             optimizer.zero_grad()
             if (i + 1) % 200 == 0:
                 print(loss.item())
-        with torch.no_grad():
-            classify.eval()
-            correct = 0.
-            total = 0.
-            for x, y in valid:
-                c = (classify(x).argmax(dim=-1) == y).sum().item()
-                t = x.size(0)
-                correct += c
-                total += t
-            acc = correct / float(total)
-            print("Validation acc:", acc,)
-            if acc > best_acc:
-                torch.save(classify, "svhn_classifier.pt")
-                print("Saved.")
-            else:
-                print()
+        acc = evaluate(classify, valid)
+        print("Validation acc:", acc,)
+
+        if acc > best_acc:
+            best_acc = acc
+            torch.save(classify, "svhn_classifier.pt")
+            print("Saved.")
+    classify = torch.load("svhn_classifier.pt")
+    print("Test accuracy:", evaluate(classify, test))
