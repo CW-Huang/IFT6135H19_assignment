c7e42c54f75ba455c0ee63cc344d9a395d0e320f
Sam <samuel.lavoie.m@gmail.com>
2019-03-22 15:57:13 -0400

    Modify batch size

--- assignment3/samplers.py
+++ assignment3/samplers.py
@@ -2,19 +2,19 @@ import numpy as np
 import random
 
 
-def distribution1(x, batch_size=1):
+def distribution1(x, batch_size=512):
     # Distribution defined as (x, U(0,1)). Can be used for question 3
     while True:
         yield(np.array([(x, random.uniform(0, 1)) for _ in range(batch_size)]))
 
 
-def distribution2(batch_size=1):
+def distribution2(batch_size=512):
     # High dimension uniform distribution
     while True:
         yield(np.random.uniform(0, 1, (batch_size, 2)))
 
 
-def distribution3(batch_size=1):
+def distribution3(batch_size=512):
     # High dimension gaussian distribution
     while True:
         yield(np.random.normal(0, 1, (batch_size, 2)))
