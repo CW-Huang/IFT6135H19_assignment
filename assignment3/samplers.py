d05d0255aeda965bc0f514cbf8c2d95ad26bcd15
Sam <samuel.lavoie.m@gmail.com>
2019-03-22 14:40:53 -0400

    2d sample

--- assignment3/samplers.py
+++ assignment3/samplers.py
@@ -11,13 +11,13 @@ def distribution1(x, batch_size=1):
 def distribution2(batch_size=1):
     # High dimension uniform distribution
     while True:
-        yield(np.random.uniform(0, 1, (batch_size, 20)))
+        yield(np.random.uniform(0, 1, (batch_size, 2)))
 
 
 def distribution3(batch_size=1):
     # High dimension gaussian distribution
     while True:
-        yield(np.random.normal(0, 1, (batch_size, 20)))
+        yield(np.random.normal(0, 1, (batch_size, 2)))
 
 
 if __name__ == '__main__':
