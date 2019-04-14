1ad1bd4f519b98aa51f855b71301dcb8fe7cfd83
yaswagner <yasmine.dumouchel@gmail.com>
2019-04-14 14:04:49 -0400

    q1

--- assignment3/Q1/density_estimation.py
+++ assignment3/Q1/density_estimation.py
@@ -8,9 +8,12 @@ Created on Sat Mar 23 13:20:15 2019
 
 
 from __future__ import print_function
+import argparse
 import numpy as np
-import torch 
+import torch
 import matplotlib.pyplot as plt
+
+from samplers import distribution1, distribution2, distribution3, distribution4
 
 # plot p0 and p1
 plt.figure()
@@ -29,15 +32,14 @@ plt.plot(f(torch.from_numpy(xx)).numpy(), d(torch.from_numpy(xx)).numpy()**(-1)*
 plt.plot(xx, N(xx))
 
 
-############### import the sampler ``samplers.distribution4'' 
+############### import the sampler ``samplers.distribution4''
 ############### train a discriminator on distribution4 and standard gaussian
 ############### estimate the density of distribution4
 
 #######--- INSERT YOUR CODE BELOW ---#######
- 
 
 
-
+    # if args.question == 4:
 
 
 
@@ -52,32 +54,21 @@ plt.plot(xx, N(xx))
 
 
 ############### plotting things
-############### (1) plot the output of your trained discriminator 
+############### (1) plot the output of your trained discriminator
 ############### (2) plot the estimated density contrasted with the true density
 
 
 
-r = xx # evaluate xx using your discriminator; replace xx with the output
-plt.figure(figsize=(8,4))
-plt.subplot(1,2,1)
-plt.plot(xx,r)
-plt.title(r'$D(x)$')
+    r = xx # evaluate xx using your discriminator; replace xx with the output
+    plt.figure(figsize=(8,4))
+    plt.subplot(1,2,1)
+    plt.plot(xx,r)
+    plt.title(r'$D(x)$')
 
-estimate = np.ones_like(xx)*0.2 # estimate the density of distribution4 (on xx) using the discriminator; 
-                                # replace "np.ones_like(xx)*0." with your estimate
-plt.subplot(1,2,2)
-plt.plot(xx,estimate)
-plt.plot(f(torch.from_numpy(xx)).numpy(), d(torch.from_numpy(xx)).numpy()**(-1)*N(xx))
-plt.legend(['Estimated','True'])
-plt.title('Estimated vs True')
-
-
-
-
-
-
-
-
-
-
-
+    estimate = np.ones_like(xx)*0.2 # estimate the density of distribution4 (on xx) using the discriminator;
+                                    # replace "np.ones_like(xx)*0." with your estimate
+    plt.subplot(1,2,2)
+    plt.plot(xx,estimate)
+    plt.plot(f(torch.from_numpy(xx)).numpy(), d(torch.from_numpy(xx)).numpy()**(-1)*N(xx))
+    plt.legend(['Estimated','True'])
+    plt.title('Estimated vs True')
--- assignment3/Q1/discriminator.py
+++ assignment3/Q1/discriminator.py
@@ -4,54 +4,38 @@ from torch.optim import SGD
 import torch.nn.functional as F
 from torch.autograd.variable import Variable
 from torch.autograd import grad as torch_grad
-
+import numpy as np
 import samplers
+
+import matplotlib.pyplot as plt
+
+
+# Using GPU if available
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
 class Discriminator(torch.nn.Module):
     """
     A three hidden-layer discriminative neural network
     """
-    def __init__(self, input_dim, hidden_dim, out_dim, dropout):
-        super(DiscriminatorNet, self).__init__()
-        n_features = 1
-        n_out = 1
+    def __init__(self, input_dim, hidden_dim, out_dim):
+        super(Discriminator, self).__init__()
 
-        self.hidden0 = nn.Sequential(
-            nn.Linear(n_features, hidden_dim[0]),
-            nn.ReLU()
-        )
-        self.hidden1 = nn.Sequential(
+        self.layers = nn.Sequential(
+            nn.Linear(input_dim, hidden_dim[0]),
+            nn.ReLU(),
             nn.Linear(hidden_dim[0], hidden_dim[1]),
-            nn.ReLU()
+            nn.ReLU(),
+            nn.Linear(hidden_dim[1], out_dim),
+            nn.ReLU(),
+            nn.Sigmoid()
         )
-        self.hidden2 = nn.Sequential(
-            nn.Linear(hidden_dim[1], hidden_dim[2]),
-            nn.ReLU()
-        )
 
+    def forward(self, x, y):
+        return self.layers(x), self.layers(y)
 
-    def forward(self, x):
-        x = self.hidden0(x)
-        x = self.hidden1(x)
-        x = self.hidden2(x)
-        return x
-
-def JSD(x, y):
-    """
-    Implementation of pairwise Jensen-Shannon Divergence
-    """
-    return np.log(2) + (0.5*torch.mean(torch.log(x)) + 0.5*torch.mean(torch.log(1 - y)))
-
-def WD(x, y, bs, l):
-    """
-    Implementation of Wasserstein Distance with gradient penalty.
-    """
-    penality = gradient_penalty(x, y, bs)
-    return -(torch.mean(x) - torch.mean(y) - l*penality)
-
-def gradient_penalty(discriminator x, y, bs):
+def gradient_penalty(D, x, y, bs):
     # Sample from Uniform distribution
-    u = samplers.distribution2(batch_size=bs)
+    dist_u = iter(distribution1(0))
     a = torch.from_numpy(next(u)).float()
     a.to(device)
 
@@ -61,7 +45,7 @@ def gradient_penalty(discriminator x, y, bs):
     z.to(device)
 
     # Compute probability of interpolated examples
-    prob_z = discriminator(z)
+    prob_z = D(z)
 
     # Compute gradients of probability w.r.t interpolated examples
     grads_z = torch_grad(outputs=prob_z, inputs=z, grad_outputs=torch.ones(prob_z.size()).cuda() if torch.cuda.is_available() else  torch.ones(prob_z.size()), create_graph=True, retain_graph=True)[0]
@@ -69,29 +53,109 @@ def gradient_penalty(discriminator x, y, bs):
     # Compute norm of gradients
     grads_norm = torch.sqrt(torch.sum(grads_z ** 2, dim=1) + 1e-12)
 
-    return ((grads_norm - 1) ** 2).mean())
+    return ((grads_norm - 1) ** 2).mean()
 
-def train(p, q, D, loss_func='JSD', lmbd=15, optimizer=SGD(lr=1e-3), n_epochs=50):
+
+def loss_JSD(x, y):
+    """
+    Implementation of Jensen-Shannon Divergence
+    """
+    return -(torch.log(torch.Tensor([2])) + 0.5*torch.mean(torch.log(x)) + 0.5*torch.mean(torch.log(1 - y)))
+
+def loss_WD(x, y, l, penalty):
+    """
+    Implementation of Wasserstein Distance with gradient penalty.
+    """
+    penality = gradient_penalty(D, x, y, bs)
+    return -(torch.mean(x) - torch.mean(y) - l*penality)
+
+def train(D, p, q, loss_metric='JSD', lmbd=35, n_epochs=2000):
     """
     Function to train the discriminator
     """
-    optimizer.zero_grad()
+    optimizer=SGD(D.parameters(), lr=1e-3)
+    D.train()
+    running_loss = 0
 
     for epoch in range(n_epochs):
+        optimizer.zero_grad()
         sample_x = torch.from_numpy(next(p)).float()
         sample_x.to(device)
         sample_y = torch.from_numpy(next(q)).float()
         sample_y.to(device)
-        x = D(sample_x)
-        y = D(sample_y)
+        x, y = D(sample_x, sample_y)
 
-        if loss_func == 'JSD':
-            x = F.sigmoid(x)
-            y = F.sigmoid(y)
-            loss = JSD(x, y)
-        elif loss_func == 'WD':
-            loss = WD(D, x, y, bs, lmbd)
+        if loss_metric == 'JSD':
+            loss = loss_JSD(x, y)
+        elif loss_metric == 'WD':
+            gp = gradient_penalty(D, x, y, 512)
+            loss = loss_WD(x, y, lmbd, gp)
+
+        running_loss += loss.item()
         loss.backward()
         optimizer.step()
+        if epoch % 500 == 0:
+            print("\tEpoch", epoch, "Loss: ", loss.data.numpy())
 
-    return D
+    return running_loss / n_epochs
+
+def estimate(D, p, q, loss_metric='JSD'):
+    """
+    Function to estimate JSD or WD of trained discriminator
+    """
+    D.eval()
+
+    sample_x = torch.from_numpy(next(p)).float()
+    sample_x.to(device)
+    sample_y = torch.from_numpy(next(q)).float()
+    sample_y.to(device)
+    x, y = D(sample_x, sample_y)
+
+    if loss_metric == 'JSD':
+        loss = loss_JSD(x, y)
+    elif loss_metric == 'WD':
+        gp = gradient_penalty(D, x, y, 512)
+        loss = loss_WD(x, y, lmbd, gp)
+
+    return loss
+
+
+if __name__ == "__main__":
+
+    # Q1.3 JSD
+    y_axis = []
+    x_axis = []
+
+    for phi in np.around(np.arange(-1.0, 1.0, 0.1), 1):
+        dist_p = samplers.distribution1(0, 512)
+        dist_q = samplers.distribution1(phi, 512)
+        D = Discriminator(2, [84,84], 1)
+        loss = train(D, dist_p, dist_q)
+        y = estimate(D, dist_p, dist_q)
+        y_axis.append(y)
+        x_axis.append(phi)
+
+    plt.plot(x_axis, y_axis, '.')
+    plt.xlim(-1, 1)
+    plt.title('{}'.format('JSD'))
+    plt.savefig('test.png')
+    plt.show()
+
+    # Q1.3 WD
+    y_axis = []
+    x_axis = []
+
+    for phi in np.around(np.arange(-1.0, 1.0, 0.1), 1):
+        dist_p = samplers.distribution1(0, 512)
+        dist_q = samplers.distribution1(phi, 512)
+        D = Discriminator(2, [84,84], 1, loss_metric='WD')
+        loss = train(D, dist_p, dist_q)
+        y = estimate(D, dist_p, dist_q)
+        y_axis.append(y)
+        x_axis.append(phi)
+
+    plt.plot(x_axis, y_axis, '.')
+    plt.xlim(-1, 1)
+    plt.title('{}'.format('JSD'))
+    plt.savefig('test.png')
+    plt.show()
