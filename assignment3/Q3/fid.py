110bb128a5006df9a33925063eab40d47750f62e
Yasmine Dumouchel Wagner <32421704+yaswagner@users.noreply.github.com>
2019-04-22 17:28:02 -0400

    Create fid.py

---
+++ assignment3/Q3/fid.py
@@ -0,0 +1,128 @@
+import argparse
+import os
+import torchvision
+import torchvision.transforms as transforms
+import torch
+import classify_svhn
+from classify_svhn import Classifier
+import numpy as np
+from numpy.linalg import norm
+from scipy.linalg import sqrtm
+
+SVHN_PATH = "svhn"
+PROCESS_BATCH_SIZE = 32
+
+
+def get_sample_loader(path, batch_size):
+    """
+    Loads data from `[path]/samples`
+    - Ensure that path contains only one directory
+      (This is due ot how the ImageFolder dataset loader
+       works)
+    - Ensure that ALL of your images are 32 x 32.
+      The transform in this function will rescale it to
+      32 x 32 if this is not the case.
+    Returns an iterator over the tensors of the images
+    of dimension (batch_size, 3, 32, 32)
+    """
+    data = torchvision.datasets.ImageFolder(
+        path,
+        transform=transforms.Compose([
+            transforms.Resize((32, 32), interpolation=2),
+            classify_svhn.image_transform
+        ])
+    )
+    data_loader = torch.utils.data.DataLoader(
+        data,
+        batch_size=batch_size,
+        num_workers=2,
+    )
+    return data_loader
+
+
+def get_test_loader(batch_size):
+    """
+    Downloads (if it doesn't already exist) SVHN test into
+    [pwd]/svhn.
+    Returns an iterator over the tensors of the images
+    of dimension (batch_size, 3, 32, 32)
+    """
+    testset = torchvision.datasets.SVHN(
+        SVHN_PATH, split='test',
+        download=True,
+        transform=classify_svhn.image_transform
+    )
+    testloader = torch.utils.data.DataLoader(
+        testset,
+        batch_size=batch_size,
+    )
+    return testloader
+
+
+def extract_features(classifier, data_loader):
+    """
+    Iterator of features for each image.
+    """
+    with torch.no_grad():
+        for x, _ in data_loader:
+            h = classifier.extract_features(x).numpy()
+            for i in range(h.shape[0]):
+                yield h[i]
+
+
+def calculate_fid_score(sample_feature_iterator,
+                        testset_feature_iterator, eps = 1e-7):
+    """
+    To be implemented by you!
+    """
+    p = []
+    q = []
+    for i in testset_feature_iterator:
+        p.append(i)
+    for i in sample_feature_iterator:
+        q.append(i)
+    
+    p = np.array(p)
+    q = np.array(q)
+
+    mu_p = np.mean(p, axis = 0)
+    mu_q = np.mean(q, axis = 0)
+
+    cov_p = np.cov(p, rowvar = False)
+    cov_q = np.cov(q, rowvar = False)
+
+    FID = norm(mu_p - mu_q)**2 + np.trace(cov_p + cov_q - 2*(sqrtm(np.matmul(cov_p, cov_q) + eps*np.identity(512))))
+
+    return FID
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description='Score a directory of images with the FID score.')
+    parser.add_argument('--model', type=str, default="svhn_classifier.pt",
+                        help='Path to feature extraction model.')
+    parser.add_argument('directory', type=str, default="fid/",
+                        help='Path to image directory')
+    args = parser.parse_args()
+
+    quit = False
+    if not os.path.isfile(args.model):
+        print("Model file " + args.model + " does not exist.")
+        quit = True
+    if not os.path.isdir(args.directory):
+        print("Directory " + args.directory + " does not exist.")
+        quit = True
+    if quit:
+        exit()
+    print("Test")
+    classifier = torch.load(args.model, map_location='cpu')
+    classifier.eval()
+
+    sample_loader = get_sample_loader(args.directory,
+                                      PROCESS_BATCH_SIZE)
+    sample_f = extract_features(classifier, sample_loader)
+
+    test_loader = get_test_loader(PROCESS_BATCH_SIZE)
+    test_f = extract_features(classifier, test_loader)
+
+    fid_score = calculate_fid_score(sample_f, test_f)
+    print("FID score:", fid_score)
